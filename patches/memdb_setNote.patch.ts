f42b70a6112f93163594b05045572b195fa0682b
wwe_oss <wayne.w.erdman@gmail.com>
2025-11-26 21:49:37 -0500

    removing old patch files

--- patches/memdb_setNote.patch.ts
+++
@@ -1,3 +0,0 @@
-// Merge this patch into your src/memdb.ts inside class MemDB:
-// --- PATCH: add setNote API ---
-  setNote(id: string, note: string) { const r = this.byId.get(id); if (r) r.notes = note; }
